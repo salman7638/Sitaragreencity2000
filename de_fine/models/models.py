8219164a9b3d5c7e2563609d163faebaef56da73
salman7638 <salman.m@dynexcel.co>
2022-04-25 11:46:18 +0000

    updated fine

--- de_fine/models/models.py
+++ de_fine/models/models.py
@@ -5,10 +5,12 @@ from odoo import models, fields, api
 class productProduct(models.Model):
     _inherit = "product.template"
     _description = "Product Product"
-    fine_amount = fields.Char(string="Fine Amount")
+    fine_amount = fields.Float(string="Fine Amount")
     
     
 class SaleOrder(models.Model):
     _inherit = "order.installment.line"
     _description = "Sale Order"
-    fine_amount = fields.Char(string="Fine Amount")
+    fine_amount = fields.Float(string="Fine Amount")
+    
+    
