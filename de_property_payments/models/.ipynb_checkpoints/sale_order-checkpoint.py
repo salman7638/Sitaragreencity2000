5fbd5a7ee231c25d71350ab477aedc9c4d90f13c
dynexcel-ramzan <m.ramzan@dynexxcel.co>
2022-04-26 05:18:50 +0000

    update property payments

--- de_property_payments/models/.ipynb_checkpoints/sale_order-checkpoint.py
+++ de_property_payments/models/.ipynb_checkpoints/sale_order-checkpoint.py
@@ -128,22 +128,7 @@ class SaleOrder(models.Model):
             if booking_amount <=0 and allotment_amount<=0: 
                 remaining_amount=total_paid_amount - (advance_amount + total_processing_fee + total_membership_fee)
                 installment_amount = (((line.amount_total)/100) * 75) -(remaining_amount)
-            if booking_amount < ((line.amount_total + total_processing_fee)/100) * 5:
-               line.update({
-                   'state': 'draft',
-               })
-               for line_prod in line.order_line:
-                    line_prod.product_id.update({
-                        'state': 'reserved',
-                    })
-            if line.allotment_amount_residual > 0 and line.booking_amount_residual <= 0:  
-                line.update({
-                    'state': 'booked',
-                })
-                for line_prod in line.order_line:
-                    line_prod.product_id.update({
-                        'state': 'booked',
-                    })                     
+                                 
             if line.amount_paid > advance_amount:
                 diff_advance_amt = total_paid_amount - advance_amount
                 installment_amount = (((line.amount_total)/100 ) * 75) - diff_advance_amt 
@@ -161,7 +146,22 @@ class SaleOrder(models.Model):
                 line.received_percent = 25
                 line.action_register_allottment()
             line.action_assign_discount()    
-            
+            if booking_amount < ((line.amount_total + total_processing_fee)/100) * 5:
+               line.update({
+                   'state': 'draft',
+               })
+               for line_prod in line.order_line:
+                    line_prod.product_id.update({
+                        'state': 'reserved',
+                    })
+            if line.allotment_amount_residual > 0 and line.booking_amount_residual <= 0:  
+                line.update({
+                    'state': 'booked',
+                })
+                for line_prod in line.order_line:
+                    line_prod.product_id.update({
+                        'state': 'booked',
+                    })
 
 
     def action_assign_discount(self):
