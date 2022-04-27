8473b48c7e37abcbeb1134ff16c0661ed5f21219
dynexcel-ramzan <m.ramzan@dynexxcel.co>
2022-04-27 04:28:28 +0000

    update bundle

--- de_fine/models/.ipynb_checkpoints/models-checkpoint.py
+++ de_fine/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,16 +1,50 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
+from datetime import date
 
 class productProduct(models.Model):
     _inherit = "product.template"
-    _description = "Product Product"
+    
     fine_amount = fields.Float(string="Fine Amount")
     
     
-class SaleOrder(models.Model):
+class OrderInstallmentLine(models.Model):
     _inherit = "order.installment.line"
-    _description = "Sale Order"
-    fine_amount = fields.Float(string="Fine Amount")
     
+    fine_amount = fields.Float(string="Fine Amount", compute='_compute_fine_amount')
     
+    @api.depends("date","total_amount")
+    def _compute_fine_amount(self):
+        for line in self:
+            today_date = date.today()
+            if today_date > line.date:
+#             raise UserErorr(date.today())
+                fine_days=0
+                f_date = date.today()
+                l_date =line.date
+                delta = f_date - l_date
+                fine_days=delta.days
+                line.fine_amount=(((((line.total_amount/100)*1)/365)*12)*fine_days) 
+            else:
+                line.fine_amount=0
+            if line.fine_amount > 0 and line.remarks=='Pending':
+                line.total_amount= line.fine_amount + line.total_amount  
+                
+                
+                
+                
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+    
+    @api.model
+    def add_fine_amount(self):
+        for rec in self:
+            total_fine_amount = 0
+            for line in rec.installment_line_ids:
+                if line.fine_amount > 0:
+                    total_fine_amount += line.fine_amount
+            rec.installment_amount_residual = total_fine_amount + rec.installment_amount_residual
+                
+                
