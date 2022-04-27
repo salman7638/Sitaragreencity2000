3b87a3b6fda4fba411f176da18dc7eb21124d22b
dynexcel-ramzan <m.ramzan@dynexcel.co>
2022-04-27 05:49:06 +0000

    update bundle

--- de_commission_report/models/models.py
+++ de_commission_report/models/models.py
@@ -7,3 +7,17 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     
     commission_date = fields.Date(string="Date")
+    co_amount = fields.Float(string = "Commission Amount", compute ='_co_amount')
+    
+    @api.depends("price_subtotal","comission_amount","commission_type")
+    def _co_amount(self):
+        for line in self:
+            if line.commission_type == 'percent':
+                line.co_amount = ((line.price_subtotal/100)*line.comission_amount)
+            elif line.commission_type == 'amount':
+                line.co_amount = line.comission_amount
+                
+            else:
+                line.co_amount = 0
+                
+                
