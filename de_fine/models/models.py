659a6335c49e14ddacc3d73ee030bc12fa2b57ef
dynexcel-ramzan <m.ramzan@dynexxcel.co>
2022-04-28 04:30:28 +0000

    update bundle

--- de_fine/models/models.py
+++ de_fine/models/models.py
@@ -15,12 +15,11 @@ class OrderInstallmentLine(models.Model):
     
     fine_amount = fields.Float(string="Fine Amount", compute='_compute_fine_amount')
     
-    @api.depends("date","total_amount")
+    @api.depends("date","total_amount","amount_residual")
     def _compute_fine_amount(self):
         for line in self:
             today_date = date.today()
             if today_date > line.date:
-#             raise UserErorr(date.today())
                 fine_days=0
                 f_date = date.today()
                 l_date =line.date
@@ -28,23 +27,29 @@ class OrderInstallmentLine(models.Model):
                 fine_days=delta.days
                 line.fine_amount=(((((line.total_amount/100)*1)/365)*12)*fine_days) 
             else:
+                line.total_amount = line.total_amount - line.fine_amount
                 line.fine_amount=0
+                
             if line.fine_amount > 0 and line.remarks=='Pending':
-                line.total_amount= line.fine_amount + line.total_amount  
+                line.total_amount= line.fine_amount + line.total_amount
+                line.amount_residual= line.fine_amount + line.amount_residual
+            elif line.fine_amount == 0:
+                line.total_amount
+              
                 
                 
                 
                 
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
+# class SaleOrder(models.Model):
+#     _inherit = "sale.order"
     
-    @api.model
-    def add_fine_amount(self):
-        for rec in self:
-            total_fine_amount = 0
-            for line in rec.installment_line_ids:
-                if line.fine_amount > 0:
-                    total_fine_amount += line.fine_amount
-            rec.installment_amount_residual = total_fine_amount + rec.installment_amount_residual
+#     @api.model
+#     def add_fine_amount(self):
+#         for rec in self:
+#             total_fine_amount = 0
+#             for line in rec.installment_line_ids:
+#                 if line.fine_amount > 0:
+#                     total_fine_amount = total_fine_amount + line.fine_amount
+#             rec.installment_amount_residual = total_fine_amount + rec.installment_amount_residual
                 
                 
